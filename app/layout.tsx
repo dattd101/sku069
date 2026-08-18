e293fa80c0b9904e390ef35afcbd52a82492ddf7
dattd101 <dattd101@gmail.com>
2026-08-18 10:11:13 +0700

    a3
    a4

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="vi" suppressHydrationWarning>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5LXCH2SC2P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5LXCH2SC2P');
+</script>
       <body suppressHydrationWarning>
         <SessionRedirect />
         {children}
